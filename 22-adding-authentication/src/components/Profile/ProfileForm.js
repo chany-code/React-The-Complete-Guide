8f4bf3cbd610abf99db0cf47dcae6fabdf7cbeba
chany-code <channn926@gmail.com>
2021-07-26 00:06:08 +0900

    Using the token for requests to protected resources

--- 22-adding-authentication/src/components/Profile/ProfileForm.js
+++ 22-adding-authentication/src/components/Profile/ProfileForm.js
@@ -1,17 +1,47 @@
+import {useRef, useContext} from "react";
+
 import classes from './ProfileForm.module.css';
+import AuthContext from "../../store/auth-context";
 
 const ProfileForm = () => {
-  return (
-    <form className={classes.form}>
-      <div className={classes.control}>
-        <label htmlFor='new-password'>New Password</label>
-        <input type='password' id='new-password' />
-      </div>
-      <div className={classes.action}>
-        <button>Change Password</button>
-      </div>
-    </form>
-  );
+    const newPasswordInputRef = useRef()
+    const authCtx = useContext(AuthContext)
+
+    const submitHandler = event => {
+        event.preventDefault()
+
+        const enteredNewPassword = newPasswordInputRef.current.value
+
+        //add validation
+
+        fetch('https://identitytoolkit.googleapis.com/v1/accounts:update?key=AIzaSyAnDXy0a5_WvabVqkRY9NaHV9XQWlJWhXk',{
+            method: 'POST',
+            body: JSON.stringify({
+                idToken: authCtx.token,
+                password: enteredNewPassword,
+                returnSecureToken: false
+            }),
+            headers: {
+                'Content-Type': 'application/json',
+                // 'Authorization': 'Bearer abc' *Check API docs!!
+            }
+        }).then(res => {
+            // assumption: Always succeeds!
+
+        })
+    }
+
+    return (
+        <form className={classes.form} onSubmit={submitHandler}>
+            <div className={classes.control}>
+                <label htmlFor='new-password'>New Password</label>
+                <input type='password' id='new-password' minLength="7" ref={newPasswordInputRef}/>
+            </div>
+            <div className={classes.action}>
+                <button>Change Password</button>
+            </div>
+        </form>
+    );
 }
 
 export default ProfileForm;
