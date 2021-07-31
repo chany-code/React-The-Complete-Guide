6357044a2b90364a394ae0951b31ce47837c2ba9
chany-code <channn926@gmail.com>
2021-07-31 17:07:02 +0900

    Adding test for user interaction and state with userEvent

--- 26-testing-react-app/src/components/Greeting.test.js
+++ 26-testing-react-app/src/components/Greeting.test.js
@@ -1,7 +1,9 @@
 import Greeting from "./Greeting";
-import {render, screen} from "@testing-library/react";
+import {fireEvent, render, screen} from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 
 describe('Greeting component', ()=>{
+
     test('renders Hello World as a text', ()=>{
         // Arrange
         render(<Greeting />)
@@ -13,4 +15,21 @@ describe('Greeting component', ()=>{
         const helloWorldElement = screen.getByText('Hello World!');
         expect(helloWorldElement).toBeInTheDocument()
     })
+
+    test('renders "It\'s good to see you!" if the button was NOT clicked', () => {
+        render(<Greeting />)
+
+        expect(screen.getByText('It\'s good to see you!')).toBeInTheDocument()
+        expect(screen.queryByText('Changed!')).not.toBeInTheDocument()
+    });
+
+    test('renders "Changed!" and hides "It\'s good to see you!" if the button was clicked', () => {
+        render(<Greeting />)
+
+        //fireEvent.click(screen.getByText('Change Text!'))
+        userEvent.click(screen.getByRole('button'))
+
+        expect(screen.queryByText('It\'s good to see you!')).not.toBeInTheDocument()
+        expect(screen.getByText('Changed!')).toBeInTheDocument()
+    });
 })
