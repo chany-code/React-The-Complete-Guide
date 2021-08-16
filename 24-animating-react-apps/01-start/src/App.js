6c213fb37572dd04baf426738d1b32f8f4390829
chany-code <channn926@gmail.com>
2021-08-16 15:09:15 +0900

    Wrapping trasition component and added animation timings

--- 24-animating-react-apps/01-start/src/App.js
+++ 24-animating-react-apps/01-start/src/App.js
@@ -31,7 +31,15 @@ class App extends Component {
                 <Transition in={this.state.showBlock}
                             timeout={1000}
                             mountOnEnter
-                            unmountOnExit>
+                            unmountOnExit
+                            onEnter={() => console.log('onEnter')}
+                            onEntering={() => console.log('onEntering')}
+                            onEntered={() => console.log('onEntered')}
+                            onExit={() => console.log('onExit')}
+                            onExiting={() => console.log('onExiting')}
+                            onExited={() => console.log('onExited')}
+                >
+
                     {state => (
                         <div style={{
                             backgroundColor: 'red',
@@ -42,9 +50,8 @@ class App extends Component {
                             opacity: state === 'exiting' ? 0 : 1
                         }}/>)}
                 </Transition>
-                {this.state.modalIsOpen ?
-                    <Modal show={this.state.modalIsOpen} closed={this.closeModal}/> : null}
-                {this.state.modalIsOpen ? <Backdrop show={this.state.modalIsOpen}/> : null}
+                <Modal show={this.state.modalIsOpen} closed={this.closeModal}/>
+                {this.state.modalIsOpen ? <Backdrop show/> : null}
                 <button className="Button" onClick={this.showModal}>Open Modal</button>
                 <h3>Animating Lists</h3>
                 <List/>
